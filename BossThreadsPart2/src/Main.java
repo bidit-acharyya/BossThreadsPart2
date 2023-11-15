a2fa83b7a50cb9d85ac5c2de1deaa48ac99913fb
bidit-acharyya <105981837+bidit-acharyya@users.noreply.github.com>
2023-11-15 14:09:14 -0500

    Implemented counting up to 100,000,000

---
+++ BossThreadsPart2/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main
+{
+    static AtomicInteger total = new AtomicInteger(0);
+    static long startTime;
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        ArrayList<Future<Integer>> futures = new ArrayList<>();
+
+        for(int i = 0; i < 100; i++)
+        {
+            Future<Integer> future = executorService.submit((Callable<Integer>) () -> {
+                AtomicInteger localSum = new AtomicInteger(0);
+                for(int j = 0; j < 1000000; j++)
+                {
+                    localSum.getAndAdd(1);
+                }
+                return localSum.get();
+            });
+            futures.add(future);
+        }
+
+        for(Future future : futures)
+        {
+            total.getAndAdd((Integer) future.get());
+        }
+        System.out.println(total.get());
+
+
+    }
+}
